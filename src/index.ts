019086fb34ad10e578a2eb844539e24bdf164939
Isaac Tewolde <isaac@tewolde.co>
2025-06-13 19:13:07 +0200

    fixed export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { useAutoObserver } from './composables/useAutoObserver'
 import { useInfiniteList } from './composables/useInfiniteList'
 import InfiniteCarousel from './components/InfiniteCarousel.vue'
+import Ghost from './components/Ghost.vue'
 // export * from './useInfiniteList'
 // export * from './useAutoObserver'
 // export { default as InfiniteCarousel } from './InfiniteCarousel.vue'
 
-export { useAutoObserver, useInfiniteList, InfiniteCarousel }
+export { useAutoObserver, useInfiniteList, InfiniteCarousel, Ghost }
