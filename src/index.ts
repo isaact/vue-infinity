c7576d8c94cddfb4e9f4a9a21f24083922f52534
Isaac Tewolde <isaac@tewolde.co>
2025-07-03 07:40:36 +0200

    bunped version, cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,5 @@ import { useInfiniteList } from './composables/useInfiniteList'
 import InfiniteCarousel from './components/InfiniteCarousel.vue'
 import Ghost from './components/Ghost.vue'
 import vGhost from './directives/vGhost'
-// export * from './useInfiniteList'
-// export * from './useAutoObserver'
-// export { default as InfiniteCarousel } from './InfiniteCarousel.vue'
 
 export { useAutoObserver, useInfiniteList, InfiniteCarousel, Ghost, vGhost }
